ac21c0fdb4c590cb76f98707c469b61e49d07902
Lorenzo <tunedconsulting@gmail.com>
2015-01-21 01:22:46 +0100

    improved Forced Network GraphViz

--- static/forcednet.js
+++ static/forcednet.js
@@ -1,63 +1,80 @@
 /**
  * Created by lorenzo on 20/01/15.
  */
+//Constants for the SVG
+//Constants for the SVG
+var width = 750,
+    height = 500;
 
-
-var width = 850,
-    height = 600;
-
+//Set up the colour scale
 var color = d3.scale.category20();
-     d3.behavior.zoom().translate([50,150]).scale(.1);
 
+function zoom() {
+    //console.log("here", d3.event.translate, d3.event.scale);
+    svg.attr("transform", "translate(" + d3.event.translate + ")scale(" + d3.event.scale + ")");
+}
+
+//d3.behavior.zoom().translate([50,150]).scale(.1);
+
+//Set up the force layout
 var force = d3.layout.force()
-    .charge(-120)
-    .linkDistance(30)
+    .linkDistance(200)
+    .linkStrength(0.3)
     .size([width, height]);
 
+//Append a SVG to the body of the html page. Assign this SVG as an object to svg
 var svg = d3.select("#diagram-1").append("svg")
     .attr("width", width)
     .attr("height", height)
     //.append("g")
     .call(d3.behavior.zoom().on("zoom", zoom))
-    .on("dblclick.zoom", null)
-    .append("g")
+    .append("g");
 
-force
-    .nodes(graph.nodes)
+svg.append("defs").selectAll("marker")
+    .data(["suit", "licensing", "resolved"])
+  .enter().append("marker")
+    .attr("id", function(d) { return d; })
+    .attr("viewBox", "0 -5 10 10")
+    .attr("refX", 25)
+    .attr("refY", 0)
+    .attr("markerWidth", 6)
+    .attr("markerHeight", 6)
+    .attr("orient", "auto")
+  .append("path")
+    .attr("d", "M0,-5L10,0L0,5 L10,0 L0, -5")
+    .style("stroke", "#4679BD")
+    .style("opacity", "0.6");
+
+
+//Creates the graph data structure out of the json data
+force.nodes(graph.nodes)
     .links(graph.links)
     .start();
 
+//Create all the line svgs but without locations yet
 var link = svg.selectAll(".link")
     .data(graph.links)
-    //.call(d3.behavior.zoom().scaleExtent([1, 8]).on("zoom", zoom)).on("dblclick.zoom", null)
     .enter().append("line")
     .attr("class", "link")
-    .style("stroke-width", function(d) { return Math.sqrt(1); });
+    .style("marker-end",  "url(#suit)") // Modified line
+    .style("stroke-width", function (d) {
+        return Math.sqrt(1);
+    });
 
+
+//Do the same with the circles for the nodes - no
 var node = svg.selectAll(".node")
     .data(graph.nodes)
-    //.call(d3.behavior.zoom().scaleExtent([1, 8]).on("zoom", zoom)).on("dblclick.zoom", null)
     .enter().append("circle")
     .attr("class", "node")
-    .attr("r", function(d) {
-        if (d.group == 'Remind') {
-        if (d.relateds == 0) return Math.sqrt(100)
-        else return (d.relateds > 5) ? Math.sqrt(5*100) : Math.sqrt(d.relateds*100);
-    }
-    else if(d.group == 'Tag') return (d.relateds > 11) ? Math.sqrt(11*35) : Math.sqrt(d.relateds*35)
-    else return Math.sqrt(20)
+    .attr("r", 8)
+    .style("fill", function (d) {
+        return color(d.type);
     })
+    .call(force.drag)
     .style("fill", function(d) { return color(d.type); })
-    .on("click", function(d,e) {
-        e.defaultPrevented
-        //console.log($('#'+d.name));
-        var obj = $('#'+d.name)
-        if (obj.attr('data')=='rem') highlight = 'highlight-blue' //'rgb(31, 119, 180)'
-        if (obj.attr('data')=='tag') highlight = 'highlight-orange' //'rgb(255, 127, 14)'
-        if (obj.attr('highlight') == 'true') { obj.removeClass(highlight);obj.attr('highlight', 'false');}
-        else { obj.addClass(highlight).fadeIn(3800); obj.attr('highlight', 'true');if(!isOnScreen(obj)) obj.scrollTo(500);}
-    })
-    .call(force.drag);
+    .on('click', connectedNodes); //Added code
+
 
     node.append("title")
         .text(function(d) { return d["name"]; });
@@ -70,25 +87,74 @@ var node = svg.selectAll(".node")
     node.attr("cx", function(d) { return d.x; })
         .attr("cy", function(d) { return d.y; });
     });
-    function zoom() {
-        //console.log("here", d3.event.translate, d3.event.scale);
-        svg.attr("transform", "translate(" + d3.event.translate + ")scale(" + d3.event.scale + ")");
-    }
-    function isOnScreen(element)
-    {
-        var curPos = element.offset();
-        var curTop = curPos.top;
-        var screenHeight = $(window).height();
-        return (curTop > screenHeight) ? false : true;
-    }
-    jQuery.fn.extend(
-    {
-        scrollTo : function(speed, easing)
-    {
-    return this.each(function()
-    {
-    var targetOffset = $(this).offset().top;
-    $('html,body').animate({scrollTop: targetOffset - height}, speed, easing);
+
+
+//Now we are giving the SVGs co-ordinates - the force layout is generating the co-ordinates which this code is using to update the attributes of the SVG elements
+force.on("tick", function () {
+    link.attr("x1", function (d) {
+        return d.source.x;
+    })
+        .attr("y1", function (d) {
+        return d.source.y;
+    })
+        .attr("x2", function (d) {
+        return d.target.x;
+    })
+        .attr("y2", function (d) {
+        return d.target.y;
     });
-    }
+
+    node.attr("cx", function (d) {
+        return d.x;
+    })
+        .attr("cy", function (d) {
+        return d.y;
     });
+});
+
+//---Insert-------
+
+//Toggle stores whether the highlighting is on
+var toggle = 0;
+
+//Create an array logging what is connected to what
+var linkedByIndex = {};
+for (i = 0; i < graph.nodes.length; i++) {
+    linkedByIndex[i + "," + i] = 1;
+};
+graph.links.forEach(function (d) {
+    linkedByIndex[d.source.index + "," + d.target.index] = 1;
+});
+
+//This function looks up whether a pair are neighbours
+function neighboring(a, b) {
+    return linkedByIndex[a.index + "," + b.index];
+}
+
+function connectedNodes() {
+    if (d3.event.defaultPrevented) return;
+    if (toggle == 0) {
+        //Reduce the opacity of all but the neighbouring nodes
+        d = d3.select(this).node().__data__;
+        node.style("opacity", function (o) {
+            return neighboring(d, o) | neighboring(o, d) ? 1 : 0.1;
+        });
+
+        link.style("opacity", function (o) {
+            return d.index==o.source.index | d.index==o.target.index ? 1 : 0.1;
+        });
+
+        //Reduce the op
+
+        toggle = 1;
+    } else {
+        //Put them back to opacity=1
+        node.style("opacity", 1);
+        link.style("opacity", 1);
+        toggle = 0;
+    }
+
+}
+
+
+
